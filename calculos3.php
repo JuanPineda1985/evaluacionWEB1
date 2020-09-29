9a0d1e6fb843ed6d484af8bc6afe7ca81d5468a1
JuanPineda1985 <71187053+JuanPineda1985@users.noreply.github.com>
2020-09-29 16:41:50 -0500

    Update calculos3.php

--- calculos3.php
+++ calculos3.php
@@ -2,21 +2,22 @@
 $n1 = $_GET['n1'];
 $n2 = $_GET['n2'];
 $imc = $n1 / $n2;
-if($imc < 18,5){
+
+if($imc < 18){
     echo "Peso insuficiente ";
-}elseif ($imc == 18,5 && $imc <= 24,9) {
+}elseif ($imc == 18 && $imc <= 24) {
     echo "peso normal ";
-}elseif ($imc == 24,9 && $imc <= 26,9) {
+}elseif ($imc == 24 && $imc <= 27) {
     echo "sobrepeso grado 1 "; 
-}elseif ($imc == 27 && $imc <= 29,9) {
-    echo "sobrepeso grado 2 (preobesidad) ";
-}elseif ($imc == 30 && $imc <= 34,9) {
+}elseif ($imc == 28 && $imc <= 30) {
+    echo "sobrepeso grado 2 (preobesidad)";
+}elseif ($imc == 31 && $imc <= 35) {
     echo "obesidad tipo 1 ";
-}elseif ($imc == 35 && $imc <= 39,9) {
+}elseif ($imc == 36 && $imc <= 39) {
     echo "obesidad tipo 2 ";
-}elseif ($imc == 40 && $imc <= 49,9) {
+}elseif ($imc == 40 && $imc <= 49) {
     echo "obesidad tipo 3 Morbida ";
-{elseif ($imc > 50) {
+{else ($imc > 50) {
     echo "obesidad tipo 4 Extrema ";
 }
 
