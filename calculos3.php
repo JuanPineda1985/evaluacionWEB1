36b62cde14c99003488e42b824cad3a4989cc6e5
JuanPineda1985 <71187053+JuanPineda1985@users.noreply.github.com>
2020-10-03 11:06:12 -0500

    continuacion

--- calculos3.php
+++ calculos3.php
@@ -4,21 +4,23 @@ $n2 = $_GET['n2'];
 $Alt = $n2*$n2;
 $imc = $n1/$Alt;
 
+echo ("tu indice de masa corporal es: ".$imc)
+
 if($imc<18){
     echo "Peso insuficiente ";
 }elseif ($imc>=24) {
     echo "peso normal ";
-}elseif ($imc == 25 && $imc <= 27) {
+}elseif ($imc >= 25 && $imc<27) {
     echo "sobrepeso grado 1 "; 
-}elseif ($imc == 28 && $imc <= 30) {
+}elseif ($imc >= 28 && $imc<30) {
     echo "sobrepeso grado 2 (preobesidad)";
-}elseif ($imc == 31 && $imc <= 35) {
+}elseif ($imc >= 31 && $imc<35) {
     echo "obesidad tipo 1 ";
-}elseif ($imc == 36 && $imc <= 39) {
+}elseif ($imc >= 36 && $imc<39) {
     echo "obesidad tipo 2 ";
-}elseif ($imc == 40 && $imc <= 49) {
+}elseif ($imc >= 40 && $imc<49) {
     echo "obesidad tipo 3 Morbida ";
-}elseif ($imc > 50) {
+}elseif ($imc >= 50) {
     echo "obesidad tipo 4 Extrema ";
 }
 
